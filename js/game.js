bca28cc2b9a393d0f2b69af22cb6cf8cb6a0c9b6
Malin <malininc.s@gmail.com>
2015-05-27 11:38:57 +0200

    bokstäver skrivs i respektive ruta

--- js/game.js
+++ js/game.js
@@ -9,6 +9,14 @@ var button = document.getElementsByTagName("button")[0];
 //binda till knappen
 button.addEventListener("click", function() {
   var anvandarOrd = document.getElementsByTagName("input")[0].value;
+    
+    //hämtar alla td
+    var rutor = document.getElementsByTagName("td");
+    
+    for(var i = 0; i<5 ; i++){
+       rutor[i].innerHTML = anvandarOrd[i];    
+    }
+       
     if(valtOrd === anvandarOrd)
     {
         alert("Grattis!");    
